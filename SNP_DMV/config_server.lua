e9735e4adb74f8de70cd3effd7b4b0f4be02f14d
SergeantNorth <sergeantnorthprod@gmail.com>
2022-07-06 05:41:14 -0600

    Add files via upload

---
+++ SNP_DMV/config_server.lua
@@ -0,0 +1,17 @@
+-- CREDITS | DO NOT REMOVE --
+-- Created by SergeantNorth#1650 --
+-- Copyright SergeantNorth Productions --
+-- discord.gg/sergeantnorth --
+
+config_s = {}
+
+-- HAMZ CAD -- 
+config_s.use_hamz_cad = false
+config_s.website_url = "https://cad.hamz.dev/" -- make sure there is a slash at the end
+config_s.secret_key = "apitest123"
+
+-- SONORAN CAD -- 
+config_s.use_sonorancad = false 
+config_s.sonoranLink = "https://api.sonorancad.com/" -- must have a / at the end
+config_s.sonorancad_id = "UQ40L6J" -- this is the ID. This can be acquired from your sonorancad admin panel.  
+config_s.sonorancad_apiKey = "" -- this is the api key. This can be acquired from your sonorancad admin panel.
